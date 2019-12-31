fb02c73feffb0dc0b0281bc2f2d7ab3dc671f840
abdullah <abdullahmafuz1@gmail.com>
2019-12-31 22:06:19 +0400

    task 1 , 2 , 3 completed

--- router/api/starwar.js
+++ router/api/starwar.js
@@ -14,7 +14,7 @@ const api=DB.conn2;
 router.get('/', async(req,res)=>{
 
     try{
-        const films=await api.collection("films").find({}).toArray();
+        const films=await api.collection("species").find({}).toArray();
  
  
  
@@ -97,7 +97,7 @@ router.get('/appearedinmost', async(req,res)=>{
         return  acc + data.characters;
     } ,[])
 
-    arrchar=character.split(',');
+   let arrchar=character.split(',');
 
   let len=arrchar.length
 
@@ -108,47 +108,14 @@ router.get('/appearedinmost', async(req,res)=>{
     arrnum.push(parseInt(arrchar[i]));
   }
 
-  //to find max reapeated number
-    function maxRepeating(arr){
+  
 
-       
-        
-       let max_count = 1;
-       let res = arr[0];
-       let curr_count = 1;
-
-       for (let i = 1; i < arr.len; i++) 
-        { 
-            if (arr[i] == arr[i - 1]) 
-                curr_count++; 
-            else 
-            { 
-                if (curr_count > max_count) 
-                { 
-                    max_count = curr_count; 
-                    res = arr[i - 1]; 
-                } 
-                curr_count = 1; 
-            } 
-        } 
-      
-      // If last element is most frequent 
-      if (curr_count > max_count) 
-      { 
-          max_count = curr_count; 
-          res = arr[n - 1]; 
-      } 
-    
-      return res; 
-
-    }
-
-let result =maxRepeating(arrnum.sort());
+let result =maxRepeating(arrnum);
 
 
 // fetching data from people db, for character (person) appeared in most of the Star Wars films by its id
 
-const people=await api.collection("people").findOne({id:result});
+const people=await api.collection("people").findOne({id:result.maxEl});
 
  
          res.json(people.name);
@@ -161,6 +128,163 @@ const people=await api.collection("people").findOne({id:result});
  }
 
 })
+
+
+
+
+
+
+
+
+
+
+
+
+router.get('/speciesappearedmost', async(req,res)=>{
+    
+   
+     
+     try{
+
+         // fetching films  data from db
+
+        const films=await api.collection("films").find({}).toArray();
+ 
+        var species=films.reduce((acc,data)=>{
+            return  acc + data.species;
+        } ,[])
+        
+        let arrchar=species.split(',');
+
+            let len=arrchar.length
+
+            var arrnum1=[]
+
+            for(let i=0;i<len;i++){
+                
+                arrnum1.push(parseInt(arrchar[i]));
+            }
+
+  
+
+var result =maxRepeating(arrnum1);
+
+
+let mostsp=result.modeMap
+
+let arryOfMaxSpec=[]
+
+for (let key in mostsp){
+    if(mostsp.hasOwnProperty(key)){
+
+        if(mostsp[key] > result.maxEl  )
+        {
+        arryOfMaxSpec.push(Number(key))
+        }
+    }
+ }
+ let newArryOfMaxSpec=[]
+ 
+
+ 
+ // fetching specices  data from db
+    let listOfSpecies =await api.collection("species").find({}).toArray();
+    
+ 
+    listOfSpecies.forEach(item =>{
+
+        if(arryOfMaxSpec.includes(item.id)){
+            let id =item.id
+            let val=item.name;
+           newArryOfMaxSpec.push({id,val});
+        }
+        
+    })
+
+    let finalarr=[]
+
+    arryOfMaxSpec=[];
+    for (let key in mostsp){
+        if(mostsp.hasOwnProperty(key)){
+    
+            if(mostsp[key] > result.maxEl  )
+            {
+                let val=mostsp[key]
+                let id=Number(key)
+            arryOfMaxSpec.push({id,val})
+            }
+        }
+     }
+ 
+
+     //combine speices data and number of apparent in films
+     
+     newArryOfMaxSpec.forEach((item,index)=>{
+
+        let size=0;
+        for (let key in arryOfMaxSpec) {
+            if (arryOfMaxSpec.hasOwnProperty(key)) 
+            size++;
+        }
+        
+        for(let i=0;i < size;i++){
+            console.log(arryOfMaxSpec[i].id)
+            if(arryOfMaxSpec[i].id == item.id){
+                let species=item.val;
+                let noOfAppeared=arryOfMaxSpec[i].val;
+                finalarr.push({species,noOfAppeared})
+            }
+        }
+            
+    
+       
+     })
+
+        res.json(finalarr);
+ 
+     }catch(err){
+ 
+         console.error(err.message)
+ 
+         res.status(500).send('server error')
+     }
+ })
+ 
+ 
+ 
+
+
+
+
+
+
+
+
+
+ 
+
+//Function to find max reapeated number
+ function maxRepeating(array)
+ {
+     if(array.length == 0)
+         return null;
+     var modeMap = {};
+     var maxEl = array[0], maxCount = 1;
+     for(var i = 0; i < array.length; i++)
+     {
+         var el = array[i];
+         if(modeMap[el] == null)
+             modeMap[el] = 1;
+         else
+             modeMap[el]++;  
+         if(modeMap[el] > maxCount)
+         {
+             maxEl = el;
+             maxCount = modeMap[el];
+         }
+     }
+     return {maxEl,maxCount,modeMap};
+ }
 
 
 
